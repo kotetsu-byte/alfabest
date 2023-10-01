c3d020e255828efaa8300b2cdbedd7245f6d71ca
kotetsu-byte <gross.23.29.11@gmail.com>
2023-10-02 03:26:27 +0500

    10/1/20203

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+let API = axios.create({baseURL : 'http://alfabest.napaautomotive.uz/'});
+
+export default API;
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="container">
+        
+    </div>
+</template>
+
+<script>
+import API from '../api';
+
+export default {
+    name: 'Home',
+    components: {},
+    data() {
+        return {
+            text: ''
+        }
+    },
+    methods: {
+        setData() {
+            var config = {
+                headers: {'Accept-Language': 'ru'}
+            }
+            API.get('/api/about_company', config)
+                .then(data => {
+                    console.log(data.data);
+                    document.getElementById("container").innerHTML =  data.data.datas[0].text_ru;
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+    },
+    mounted() {
+        this.setData();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
